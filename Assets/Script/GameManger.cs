2722b8d3b80f978ef449c8e5a80058dd565903cf
thanhtuan0310 <thanhtuan102001@gmail.com>
2022-04-06 16:45:42 +0700

    Push respo

---
+++ Assets/Script/GameManger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManger : MonoBehaviour
+{
+    public TextMeshProUGUI scoretext;
+    public TextMeshProUGUI gameovertext;
+    public List<GameObject> target;
+    private float rate = 1.0f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdargeScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(rate);
+            int index = Random.Range(0, target.Count);
+            Instantiate(target[index]);
+
+        }
+    }
+
+    public void UpdargeScore(int scoreadd)
+    {
+        score += scoreadd;
+        scoretext.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameovertext.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Script/Target.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Target : MonoBehaviour
+{
+    public int pointvalue;
+    public ParticleSystem explosion;
+    private GameManger game;
+    private Rigidbody targetRb;
+    private int minSpeed = 12;
+    private int maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawn = -2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = GameObject.Find("GameManager").GetComponent<GameManger>();
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawn, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+        Instantiate(explosion, transform.position, explosion.transform.rotation);
+        game.UpdargeScore(pointvalue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        if (!gameObject.CompareTag("Bad")) {
+            game.GameOver();
+        }
+    }
+}
